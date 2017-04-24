dea28a7b9bf310033eb0c9afc04fdddf46224dd0
Jimmy Smutek <jimmy.smutek@ainsleyagency.com>
2017-04-24 13:00:05 -0400

    Move admin styles to dedicated template

---
+++ templates/modules/admin-styles.php
@@ -0,0 +1,13 @@
+<style id="ainsley-admin-styles">
+  /* ACF Styles
+  * Helps to visually separate ACF blocks out when there's a lot of them.
+  */
+  .acf-postbox h2.hndle {
+    background: #ebebeb;
+    color: #424242;
+    font-size: 21px !important;
+  }
+  .acf-postbox.closed h2.hndle {
+    background: #dbdbdb;
+  }
+</style>
